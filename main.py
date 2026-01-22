69e4b0c7bcf05e9003fbe2ecbfb15bfad57705ee
Shaurya <shauryagajjar2626@gmail.com>
2026-01-22 17:39:05 +0530

    c

--- main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from hacking!")
-
-
-if __name__ == "__main__":
-    main()
--- src/scripts/wifi_signal.py
+++ src/scripts/wifi_signal.py
@@ -1,13 +1,22 @@
 from pywifi import PyWiFi
+import time
+import os
 
 wifi = PyWiFi()
 iface = wifi.interfaces()[0]
-iface.scan()
 
-results = iface.scan_results()
+while True:
+    iface.scan()
+    time.sleep(0.5)     
 
-print(f"{'SSID':30} {'Signal(dBm)':>12} {'BSSID':>20}")
-print("-" * 65)
+    results = iface.scan_results()
 
-for network in results:
-    print(f"{network.ssid:30} {network.signal:>12} {network.bssid:>20}")
+    if results:
+        os.system("cls") 
+        print(f"{'SSID':30} {'Signal(dBm)':>12} {'BSSID':>20}")
+        print("-" * 65)
+
+        for network in results:
+            print(f"{network.ssid:30} {network.signal:>12} {network.bssid:>20}")
+    else:
+        print("No networks found")
