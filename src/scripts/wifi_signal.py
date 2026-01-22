8bbcaef96f774264828044d3f95987a9b7d87ad8
Shaurya <shauryagajjar2626@gmail.com>
2026-01-22 17:58:45 +0530

    c

--- src/scripts/wifi_signal.py
+++ src/scripts/wifi_signal.py
@@ -9,14 +9,19 @@ while True:
     iface.scan()
     time.sleep(0.5)     
 
-    results = iface.scan_results()
+    networks = iface.scan_results()
+    
+    seen = set()
+    unique_networks = [n for n in networks if n.ssid not in seen and not seen.add(n.ssid)]
+    
+    networks = unique_networks
 
-    if results:
+    if networks:
         os.system("cls") 
         print(f"{'SSID':30} {'Signal(dBm)':>12} {'BSSID':>20}")
         print("-" * 65)
 
-        for network in results:
+        for network in networks:
             print(f"{network.ssid:30} {network.signal:>12} {network.bssid:>20}")
     else:
         print("No networks found")
