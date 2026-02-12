3873e6ed056586d2412a9ce4861b4405f472a245
Shaurya Gajjar <shauryagajjar2626@gmail.com>
2026-02-12 10:44:13 +0530

    added

--- src/shell/ram_moniter.sh
+++ src/shell/ram_moniter.sh
@@ -4,7 +4,7 @@ clear
 freeRam=$(free -mt | grep "Total" | awk '{print $4}')
 threshold=500
 
-if [ $freeRam -lt $threshold ]; then
+if [ "$freeRam" -lt $threshold ]; then
     echo "Warning, RAM is running low, free RAM is $freeRam MB"
 else
     echo "free RAM is $freeRam MB"
--- src/shell/test.sh
+++ src/shell/test.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 clear
 
-ans=$(($1 + $2))
+age="$1"
+skill="$2"
 
-echo "$ans"
+if [ "$age" -ge 18 ] && [ "$skill" = "true" ]; then
+    echo "You got licence"
+
+elif [ "$age" -ge 18 ]; then
+    echo "You got skill"
+
+else 
+    echo "Be 18 years old"
+
+fi    
